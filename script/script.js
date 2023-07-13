525b4ade6c30152a010fbabb88d8b31c1fd6d2a8
Dave <da>
2023-07-13 23:15:24 +0200

    Add points and refactoring

--- script/script.js
+++ script/script.js
@@ -1,104 +1,104 @@
-const questions =[
+const questions = [
     {
-        question:"Jaké je datum narození tvé mamky?",
-        answers:[
-            {text:"31.5.1982",correct:false},
-            {text:"31.5.1983",correct:false},
-            {text:"30.5.1983",correct:true},
-            {text:"30.5.1984",correct:false}
+        question: "Jaké je datum narození tvé mamky?",
+        answers: [
+            { text: "31.5.1982", correct: false },
+            { text: "31.5.1983", correct: false },
+            { text: "30.5.1983", correct: true },
+            { text: "30.5.1984", correct: false }
         ]
     },
     {
-        question:"Jaké bylo příjmení tvé mamky za svobodna?",
-        answers:[
-            {text:"Prudičová",correct:false},
-            {text:"Tomková",correct:true},
-            {text:"Pazderová",correct:false},
-            {text:"Tomková-Prudičová",correct:false}
+        question: "Jaké bylo příjmení tvé mamky za svobodna?",
+        answers: [
+            { text: "Prudičová", correct: false },
+            { text: "Tomková", correct: true },
+            { text: "Pazderová", correct: false },
+            { text: "Tomková-Prudičová", correct: false }
         ]
     },
     {
-        question:"Jak se jmenuje sourozenec zetě tojí babičky?",
-        answers:[
-            {text:"Robert",correct:false},
-            {text:"Ivan",correct:false},
-            {text:"Jitka",correct:false},
-            {text:"David",correct:true}
+        question: "Jak se jmenuje sourozenec zetě tojí babičky?",
+        answers: [
+            { text: "Robert", correct: false },
+            { text: "Ivan", correct: false },
+            { text: "Jitka", correct: false },
+            { text: "David", correct: true }
         ]
     },
     {
-        question:"Jaký je výsledek toho příkladu: 333:3+(-40·2)-(-4²)-4",
-        answers:[
-            {text:"0",correct:false},
-            {text:"1",correct:false},
-            {text:"11",correct:true},
-            {text:"100",correct:false}
+        question: "Jaký je výsledek toho příkladu: 333:3+(-40·2)-(-4²)-4",
+        answers: [
+            { text: "0", correct: false },
+            { text: "1", correct: false },
+            { text: "11", correct: true },
+            { text: "100", correct: false }
         ]
     },
     {
-        question:"Která hora je nejvyšší v České republice",
-        answers:[
-            {text:"Lysá hora",correct:false},
-            {text:"Sněžka",correct:true},
-            {text:"Mount Everest",correct:false},
-            {text:"Smrk",correct:false}
+        question: "Která hora je nejvyšší v České republice",
+        answers: [
+            { text: "Lysá hora", correct: false },
+            { text: "Sněžka", correct: true },
+            { text: "Mount Everest", correct: false },
+            { text: "Smrk", correct: false }
         ]
     },
     {
-        question:"Vyber správnou možnost do následující věty: Spatřil jsem ve vodě ___ a na stromě ___.",
-        answers:[
-            {text:"vír, víra",correct:false},
-            {text:"vír, výra",correct:true},
-            {text:"výr, víra",correct:false},
-            {text:"výr, výra",correct:false}
+        question: "Vyber správnou možnost do následující věty: Spatřil jsem ve vodě ___ a na stromě ___.",
+        answers: [
+            { text: "vír, víra", correct: false },
+            { text: "vír, výra", correct: true },
+            { text: "výr, víra", correct: false },
+            { text: "výr, výra", correct: false }
         ]
     },
     {
-        question:"Celý život se inspiroval zážitky z dětství a venkovského života v rodných Hrusicích nedaleko Prahy. Jistě si vzpomeneš na jeho kocoura Mikeše",
-        answers:[
-            {text:"M. Aleš",correct:false},
-            {text:"J. Lada",correct:true},
-            {text:"J. Karafiát",correct:false},
-            {text:"J. Čapek",correct:false}
+        question: "Celý život se inspiroval zážitky z dětství a venkovského života v rodných Hrusicích nedaleko Prahy. Jistě si vzpomeneš na jeho kocoura Mikeše",
+        answers: [
+            { text: "M. Aleš", correct: false },
+            { text: "J. Lada", correct: true },
+            { text: "J. Karafiát", correct: false },
+            { text: "J. Čapek", correct: false }
         ]
     },
     {
-        question:"Vyber možnost, která je desetkrát menší než 1,2t.",
-        answers:[
-            {text:"1,2kg",correct:false},
-            {text:"120kg",correct:true},
-            {text:"1,2g",correct:false},
-            {text:"1,2mg",correct:false}
+        question: "Vyber možnost, která je desetkrát menší než 1,2t.",
+        answers: [
+            { text: "1,2kg", correct: false },
+            { text: "120kg", correct: true },
+            { text: "1,2g", correct: false },
+            { text: "1,2mg", correct: false }
         ],
     },
     {
-        question:"Kdy má svátek tvůj děda nebo taťka?",
-        answers:[
-            {text:"25.června",correct:true},
-            {text:"27.srpna",correct:false},
-            {text:"27.6.",correct:false},
-            {text:"25.července",correct:false}
+        question: "Kdy má svátek tvůj děda nebo taťka?",
+        answers: [
+            { text: "25.června", correct: true },
+            { text: "27.srpna", correct: false },
+            { text: "27.6.", correct: false },
+            { text: "25.července", correct: false }
         ]
     },
     {
-        question:"Před školou v Velkém Újezdě máte bystu, koho je?",
-        answers:[
-            {text:"Vladimíra Železného",correct:false},
-            {text:"Emila Zátopka",correct:false},
-            {text:"Emila Navrátila",correct:false},
-            {text:"Vladimíra Navrátila",correct:true}
+        question: "Před školou v Velkém Újezdě máte bystu, koho je?",
+        answers: [
+            { text: "Vladimíra Železného", correct: false },
+            { text: "Emila Zátopka", correct: false },
+            { text: "Emila Navrátila", correct: false },
+            { text: "Vladimíra Navrátila", correct: true }
         ]
     },
     {
-        question:"Vyber zápis čísla π, který mu bude nejblíže",
-        answers:[
-            {text:"3,14159264358979323846",correct:false},
-            {text:"3,14159265358979322846",correct:false},
-            {text:"3,14159265358979323846",correct:true},
-            {text:"3,14159265358679323846",correct:false}
+        question: "Bonusová otázka za 100 bodů.<br/>Vyber zápis čísla π, který mu bude nejblíže",
+        answers: [
+            { text: "3,14159264358979323846", correct: false },
+            { text: "3,14159265358979322846", correct: false },
+            { text: "3,14159265358979323846", correct: true },
+            { text: "3,14159265358679323846", correct: false }
         ]
     }
-    
+
 ];
 
 const questionElement = document.getElementById("question");
@@ -108,62 +108,59 @@ const nextButton = document.getElementById("btn-next");
 let score = 0;
 
 // Start Quiz
-function startQuiz()
-{
+function startQuiz() {
     score = 0;
     nextButton.innerHTML = "Další";
     showQuestion();
 }
 
 // Show Question
-function showQuestion()
-{
+function showQuestion() {
     resetState();
     let currentQuestionIndex = localStorage.getItem('currentQuestionIndex');
-    let currentQuestion= questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo+". "+currentQuestion.question;
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = Number(currentQuestionIndex) + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButton.appendChild(button);
-        if(answer.correct)
-        {
+        if (answer.correct) {
             button.dataset.correct = answer.correct;
         }
-        button.addEventListener("click",selectAnswer);
-    
+        button.addEventListener("click", selectAnswer);
+
     });
 }
 
 // Reset State
-function resetState()
-{
+function resetState() {
     nextButton.style.display = "none";
-    while(answerButton.firstChild)
-    {
+    while (answerButton.firstChild) {
         answerButton.removeChild(answerButton.firstChild);
     }
 }
 
 // Select Answer
-function selectAnswer(e)
-{
+function selectAnswer(e) {
     const selectedbtn = e.target;
     const isCorrect = selectedbtn.dataset.correct === "true";
-    if(isCorrect)
-    {
+    if (isCorrect) {
         selectedbtn.classList.add("correct");
-        score++;
+        let currentQuestionIndex = localStorage.getItem('currentQuestionIndex');
+
+        if (currentQuestionIndex == 10)
+            score += 100;
+        else
+            score += 50;
     }
-    else{
+    else {
         selectedbtn.classList.add("wrong");
     }
     Array.from(answerButton.children).forEach(button => {
-        if(button.dataset.correct === "true")
-        {
+        if (button.dataset.correct === "true") {
             button.classList.add("correct");
         }
         button.disabled = true;
@@ -172,67 +169,58 @@ function selectAnswer(e)
 }
 
 // Show Score
-function showScore()
-{
+function showScore() {
     resetState();
-    questionElement.innerHTML = "Your Score is "+score+" out of "+questions.length;
-    nextButton.innerHTML = "Restart";
-    nextButton.style.display = "block";
+    questionElement.innerHTML = "Dosáhl jsi " + score + " bodů, pro odměnu a kontrolu jdi za strejdou Davidem :-).";
 }
 
 // Handle Next Button
-function handleNextButton()
-{
+function handleNextButton() {
     let currentQuestionIndex = localStorage.getItem('currentQuestionIndex');
 
     currentQuestionIndex++;
     localStorage.setItem('currentQuestionIndex', currentQuestionIndex);
 
-    if(currentQuestionIndex < questions.length)
-    {
+    if (currentQuestionIndex < questions.length) {
         showQuestion();
     }
-    else{
+    else {
         showScore();
     }
 }
 
-nextButton.addEventListener("click",() => {
+nextButton.addEventListener("click", () => {
 
     let currentQuestionIndex = localStorage.getItem('currentQuestionIndex');
 
-    if(nextButton.innerHTML == "Start")
-    {
+    if (nextButton.innerHTML == "Start") {
         localStorage.setItem('start', true);
         startQuiz()
 
-    }else if(currentQuestionIndex < questions.length )
-    {
+    } else if (currentQuestionIndex < questions.length) {
         handleNextButton();
     }
-    else{
+    else {
         startApp();
     }
 });
 
 
 // Start app
-function startApp()
-{
+function startApp() {
 
-    if(localStorage.getItem('start'))
-    {
+    if (localStorage.getItem('start')) {
         startQuiz();
 
-    }else
-    {
-        if(localStorage.getItem('currentQuestionIndex') == "undefined" || localStorage.getItem('currentQuestionIndex') == null )
+    } else {
+        if (localStorage.getItem('currentQuestionIndex') == "undefined" || localStorage.getItem('currentQuestionIndex') == null) {
             localStorage.setItem('currentQuestionIndex', 0);
+            localStorage.setItem('score', 0);
+        }
         score = 0;
         questionElement.innerHTML = "Ukaž co dokážeš.<br /> Za každou správnou odpověď dostaneš 50bodů.<br />Za každý bod po skončení kvízu dostaneš jednu korunu. <br /> Kvíz je možné hrát jen jednou!!!";
         nextButton.innerHTML = "Start";
         nextButton.style.display = "block";
-
     }
 }
 
